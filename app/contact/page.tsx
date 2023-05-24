d345d13794a13024b2d2c04a81281445803a3e16
FilipJQ77 <filipprzygonski99@gmail.com>
2023-05-24 23:11:21 +0200

    Add technologies, experience, contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,18 @@
-export default function Contact(){
-    return(
-        <>
-        Email: mail@mail.com
-        </>
-    )
+"use client";
+
+import { Card, CardBody, CardHeader, Heading, Text } from "@chakra-ui/react";
+
+export default function Contact() {
+  return (
+    <Card m={3} minW="20em" w="40%">
+      <CardHeader>
+        <Heading size="md" className="text-purple-500">
+          Contact me:
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>My work e-mail address: fprzygonski.dev@gmail.com</Text>
+      </CardBody>
+    </Card>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,16 @@ import {
   Center,
   Image,
   Flex,
+  SimpleGrid,
   Spacer,
   Text,
   VStack,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
@@ -31,7 +38,7 @@ export default function Home() {
             </Text>
           </VStack>
         </Center>
-        <Box>
+        <Box w="100%">
           <Text pt={10} className="text-3xl font-bold text-blue-500">
             About me
           </Text>
@@ -50,8 +57,153 @@ export default function Home() {
             </Text>
           </Box>
         </Box>
+        <Box w="100%">
+          <Text pt={10} className="text-3xl font-bold text-blue-500">
+            Technologies and Experience
+          </Text>
+          <Box pt={5}>
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+            >
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    C# and .NET
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    Professional work since 2021, personal on and off since
+                    2018. I started with console, WinForms and WPF apps, then
+                    with more experience - web applications. In professional
+                    environment I mainly worked on microservices, ASP.NET MVC,
+                    ASP.NET Web API, Blazor WebAssembly. Along with that I have
+                    experience with Entity Framework. What I can also say is I
+                    developed (or at least tried) some games in the Unity
+                    Engine. I would say it is my favourite language, at least
+                    for now ;)
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Python
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    Created a wiki type application for small teams for the
+                    Innovative Projects by Nokia initiative, as well as a 2D
+                    racing game with a trained AI player for my engineering
+                    thesis. Apart from that, I mainly used Python for personal
+                    and university projects since 2017. In those, I used
+                    packages and libraries like FastAPI, Pandas, NumPy,
+                    Matplotlib, SciPy, OpenCV, PyTorch, PyGame. Personally, I
+                    like Python and would like to use it more in professional
+                    environments, especially for Data Science and AI projects.
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Databases
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    I worked with many databases, starting with MySQL. However
+                    throughout my career I mostly used MS SQL Server and
+                    PostgreSQL. I also worked with MongoDB and TinyDB as some
+                    NoSQL DBs, however I would say the favourite one to work
+                    with for me is Postgres and would choose it for any of my
+                    projects.
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Web development
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    HTML, CSS, JS was my first experience with programming of
+                    some kind back in 2015. I did not develop many websites
+                    until 2020 when I started learning React. I created some
+                    websites mostly for university courses and with time I
+                    realised I want to be a full-stack developer. Later I
+                    learned Blazor, which I used for a web app for a company
+                    project, however now I would say my favourite frontend
+                    technology is Next.js (and that's why this website is in
+                    it).
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Testing
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    I believe that any code you write needs testing. Personally
+                    I would say I like using a TDD approach for writing code and
+                    testing it. As for the tech, I used NUnit, xUnit for C#,
+                    unittest for Python, along with Selenium for testing some
+                    web apps.
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Mobile development
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    In the last semester of my engineering's degree I had a
+                    course about developing a mobile app. Our group decided to
+                    use Flutter for it, and I immediately loved it. In around 2
+                    months we created a Scrabble clone that had pretty much full
+                    functionality. After that I worked a bit on a personal
+                    project, but I've put it on a hiatus. Apart from Flutter I
+                    was looking into Kotlin Native, because I heard good
+                    opinions about the language itself.
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading size="lg" className="text-purple-500">
+                    Other languages
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>
+                    My first experience with non-web programming was with C++ in
+                    2016. At the time I loved solving problems with it, but
+                    nowadays I would not use it anywhere. I had some experience
+                    with Java, but once I learned more about C#, I never touched
+                    it again. Because of Flutter I know how Dart language works.
+                    Recently I am learning Elixir in spare time because I think
+                    knowing functional paradigm helps a lot while writing code.
+                    I want to know more about Rust, because of the love it
+                    receives from its developers. And as mentioned before Kotlin
+                    is also a language I considered doing a project in.
+                  </Text>
+                </CardBody>
+              </Card>
+            </SimpleGrid>
+          </Box>
+        </Box>
       </VStack>
-      <h2 className="ml-3 mt-24 font-bold text-gray-700 text-3xl">
+      {/* <h2 className="ml-3 mt-24 font-bold text-gray-700 text-3xl">
         My Projects
       </h2>
 
@@ -67,7 +219,7 @@ export default function Home() {
             </div>
           </Link>
         ))}
-      </div>
+      </div> */}
     </>
   );
 }
